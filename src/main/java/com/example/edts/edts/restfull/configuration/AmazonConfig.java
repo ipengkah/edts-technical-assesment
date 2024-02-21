b39030451610e555a1c4b6dd2a4f5dbdbf6ab291
ipengkah <mivanmaulana1@gmail.com>
2024-02-21 16:12:33 +0700

    add aws configuration

---
+++ src/main/java/com/example/edts/edts/restfull/configuration/AmazonConfig.java
@@ -0,0 +1,35 @@
+package com.example.edts.edts.restfull.configuration;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+    @Value("${cloud.aws.credentials.access-key}")
+    private String AWS_ACCESS_KEY_ID;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String AWS_SECRET_ACCESS_KEY;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+    @Bean
+    public AmazonS3 s3Client() {
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                AWS_ACCESS_KEY_ID,
+                AWS_SECRET_ACCESS_KEY
+        );
+
+        return AmazonS3ClientBuilder.standard().
+                withCredentials(
+                        new AWSStaticCredentialsProvider
+                                (awsCredentials)).withRegion(region).build();
+    }
+}
