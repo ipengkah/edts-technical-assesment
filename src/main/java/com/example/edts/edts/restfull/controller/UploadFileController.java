f8e17470b00de1724c04f15221730342c5eeaa11
ipengkah <mivanmaulana1@gmail.com>
2024-02-21 16:12:57 +0700

    add upload file controller

---
+++ src/main/java/com/example/edts/edts/restfull/controller/UploadFileController.java
@@ -0,0 +1,43 @@
+package com.example.edts.edts.restfull.controller;
+
+import com.example.edts.edts.restfull.dto.WebResponse;
+import com.example.edts.edts.restfull.service.UploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/file")
+public class UploadFileController {
+    @Autowired
+    private UploadService service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileName) {
+        byte[] data = service.downloadFile(fileName);
+        ByteArrayResource resource = new ByteArrayResource(data);
+        return ResponseEntity
+                .ok()
+                .contentLength(data.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{fileName}")
+    public ResponseEntity<String> deleteFile(@PathVariable String fileName) {
+        return new ResponseEntity<>(service.deleteFile(fileName), HttpStatus.OK);
+    }
+}
